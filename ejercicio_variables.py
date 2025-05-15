904f0a071d186fd62abb323b25545cf5b5b20e0a
pinceladastic <infopinceladastic@gmail.com>
2025-05-15 14:57:04 +0200

    Ejercicios de Udemy

---
+++ ejercicio_variables.py
@@ -0,0 +1,12 @@
+# EJERCICIO:
+'Dado el producto de una tienda en línea, generar 3 variables para almacenar:'
+'- nombre producto'
+'- precio producto'
+'- cantidad disponible'
+' Asignarles valores e imprimirlas por consola'
+
+nombreProducto='queso'
+precioProducto=10.75
+cantidadDisponible=80
+
+print("Nombre y precio del producto: ", nombreProducto, precioProducto,'\nCantidad Disponible: ', cantidadDisponible)
