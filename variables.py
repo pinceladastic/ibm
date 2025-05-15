63b2aecc83f19494ce382185405fdc3d811cc0e7
pinceladastic <infopinceladastic@gmail.com>
2025-05-15 14:13:27 +0200

    Definición y Asignación de Variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+'''VARIABLE: ubicación de memoria reservada donde se almacenan y manipulan datos
+   - En Python no se declaran, se crean cuando se le asigna un nombre y se les da un valor
+'''
+
+suma=1+2
+print(suma)
+
+# Asignar un valor a una variable
+a=1
+print(a)
+a=3*4
+print(a)
+a='Python' #asigna una cadena a la variable
+print(a)
+
