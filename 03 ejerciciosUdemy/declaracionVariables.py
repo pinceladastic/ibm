82663cfee21acd5e7461ff9e0e6ef92c63b0a0f8
pinceladastic <infopinceladastic@gmail.com>
2025-05-25 18:04:03 +0200

    Ejercicio de declaración de Variables

---
+++ 03 ejerciciosUdemy/declaracionVariables.py
@@ -0,0 +1,15 @@
+#Crea dos variables, la primera se llama nombre con el valor de 'Juan Perez' 
+# y la segunda variable se llama edad con el valor numérico de 30. 
+# Después manda a imprimir cada variable a consola en líneas separadas usando Python:
+# El resultado de la salida del programa debe ser el siguiente:
+# Juan Perez
+# 30
+
+# Creacion de la variable nombre con su valor
+nombre= 'Juan Perez'
+# Creacion de la variable edad con su valor
+edad=30
+# Impresion del valor de la variable nombre
+print(nombre)
+# Impresion del valor de la variable edad
+print(edad)
